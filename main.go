5dc36304ef88c4a6cda7e4092dbee0c3ac142f24
李杰(jet.li) <jet.li@huolala.cn>
2021-06-08 16:16:59 +0800

    log增加err

--- main.go
+++ main.go
@@ -866,7 +866,11 @@ func (s *DB) slog(sql string, retryIdx int, t time.Time, db interface{}, vars ..
 			}
 			logPrintFunc(err, l, customLogger(s.Error, retryIdx, t, sql, fileWithLineNum(), s.RowsAffected, db, vars...))
 		} else {
-			s.print("sql", fileWithLineNum(), NowFunc().Sub(t), sql, vars, s.RowsAffected)
+			err := s.Error
+			if err == ErrRecordNotFound {
+				err = nil
+			}
+			s.print("sql", err, fileWithLineNum(), NowFunc().Sub(t), sql, vars, s.RowsAffected)
 		}
 	}
 }
